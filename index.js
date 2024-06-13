c5a51fcc9f332d16adcfb997eb2cc2c221eb616c
Israel Santana <is.wsie@gmail.com>
2024-06-13 14:18:37 -0400

    Learned that to use ES Modules you add in the package.json file: "type": "module"

---
+++ index.js
@@ -0,0 +1,12 @@
+import { getPosts } from "./postController.js";
+
+console.log(getPosts());
+
+//CommonJS Modules Example
+//ES Modules is the modern implementation
+
+// const { generateRandomNumber, celciusToFahrenheit } = require("./utils");
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
---
+++ utils.js
@@ -0,0 +1,12 @@
+//CommonJS Modules Example
+//ES Modules is the modern implementation
+
+// function generateRandomNumber() {
+//   return Math.floor(Math.random() * 100) + 1;
+// }
+
+// function celciusToFahrenheit(celcius) {
+//   return (celcius * 9) / 5 + 32;
+// }
+
+// module.exports = { generateRandomNumber, celciusToFahrenheit };
