9875a25d6239f4e2a9620fff1041ba3e0abb674a
Israel Santana <is.wsie@gmail.com>
2024-06-14 12:24:58 -0400

    Changed the content-type to test if nodemon was working properly which it was.

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ import http from "http";
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(500, { "Content-Type": "application/json" });
-  res.end(JSON.stringify({ message: "Server Error" }));
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Learning Node JS</h1>");
 });
 
 server.listen(PORT, () => {
