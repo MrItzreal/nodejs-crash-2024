cace93cb9a7b111c933a6704ec14c63e8fa0bd47
Israel Santana <is.wsie@gmail.com>
2024-06-17 15:23:02 -0400

    IMPORTANT NOTE: I learned about url.fileURLToPath, path.dirname, and path.join for file/directory manipulation.
    While learning Node.js concepts is a valuable foundation, learning Express.js will simplify the process of building web applications and APIs.

--- server.js
+++ server.js
@@ -1,21 +1,45 @@
 //http is a module included in Node.js
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+/* These paths are incompatible with ES Modules and will cause errors if used. But you can make your own using variables and methods:
+__filename;
+__dirname;
+*/
+
+// Get current path.
+//file url is turned into a path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/*Console log both:
+__filename gives both path & file name
+__dirname only gives the path
+ console.log(__filename, __dirname);
+ */
+
+/*path.join() Joins arguments together which results in a path.*/
+
+const server = http.createServer(async (req, res) => {
   try {
     // Check if "GET" request
     if (req.method === "GET") {
+      let filePath;
       if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>Homepage</h1>");
+        filePath = path.join(__dirname, "public", "index.html");
       } else if (req.url === "/about") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end("<h1>About</h1>");
+        filePath = path.join(__dirname, "public", "about.html");
       } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end("<h1>Not Found</h1>");
+        throw new Error("Not Found");
       }
+
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
     } else {
       throw new Error("Method not allowed");
     }
