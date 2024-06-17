d03c3fa0b2781a37109c4c311e569dac391daaa1
Israel Santana <is.wsie@gmail.com>
2024-06-17 11:20:18 -0400

    Learned: to console.log req.url/req.method for debugging, and that POSTMAN is usable from VSCode. If 'localhost' fails, use '127.0.0.1' with the correct port.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import http from "http";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
+  console.log(req.url);
+  console.log(req.method);
+
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end("<h1>Learning Node JS</h1>");
 });
